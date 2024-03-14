ab2216b4dc0c4fcb147aa8c3c307d28d9c5f1c33
黄智强 <huangzq@dustess.com>
2024-03-14 23:18:00 +0800

    feat: 安装tailwindcss+headlessui+less

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import './index.less' // https://v2.tailwindcss.com/docs/guides/vue-3-vite
 
 const app = createApp(App)
 
